d2e1fb0639cf222a1b2945e2f4491e3c1e66fb40
rafael <rafaelmagalhaes1@live.com>
2024-02-14 20:47:24 -0300

    feat: add-additional created

---
+++ includes/admin/additionals/add-additional.php
@@ -0,0 +1,81 @@
+<?php
+// Check if the form was submitted
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $adicionais_data = get_option('reserva_veiculos_data_additionals', array());
+
+    // Add the new additional service to the array
+    $adicionais_data[] = array(
+        'name' => sanitize_text_field($_POST['name']),
+        'price' => sanitize_text_field($_POST['price']),
+        'description' => sanitize_text_field($_POST['description']),
+    );
+
+    // Save the updated array
+    update_option('reserva_veiculos_data_additionals', $adicionais_data);
+
+    // Redirect back to the add-ons page
+    wp_redirect(admin_url('admin.php?page=reserva_veiculos_adicionais_page'));
+}
+?>
+
+<style>
+
+    h2 {
+        border-bottom: 1px solid #ccc;
+        width: 50%;
+    }
+
+    form {
+        display: flex;
+        flex-direction: column;
+        width: 50%;
+    }
+
+    label {
+        margin-top: 10px;
+    }
+
+    input[type="text"] {
+        margin-bottom: 10px;
+    }
+
+    textarea {
+        margin-bottom: 10px;
+        height: 100px;
+    }
+
+    input[type="submit"] {
+        width: 100px;
+        padding: 10px;
+        border-radius: 5px;
+        background-color: #0073aa;
+        color: white;
+        border: none;
+        align-self: flex-end;
+    }
+
+    @media (max-width: 768px) {
+        form {
+            width: 95%;
+        }
+    }
+</style>
+
+<div>
+    <h2>Adicionar Adicional</h2>
+    <a href="<?php echo admin_url('admin.php?page=reserva_veiculos_adicionais_page'); ?>" class="page-title-action">Voltar</a>
+    <!-- Form to add a new additional service -->
+    <form method="post">
+        <label for="name">Nome:</label>
+        <input type="text" id="name" name="name" required>
+
+        <label for="price">Preço:</label>
+        <input type="text" id="price" name="price" required>
+
+        <label for="description">Descrição:</label>
+
+        <textarea id="description" name="description" required></textarea>
+
+        <input type="submit" value="Adicionar">
+    </form>
+</div>
--- includes/admin/additionals/read-additionals.php
+++ includes/admin/additionals/read-additionals.php
@@ -1,25 +1,26 @@
 <?php
-
 function reserva_veiculos_adicionais_page() {
 ?>
     <div class="container-admin">
         <?php
-            $adicionais_data = get_option('reserva_veiculos_data_additionals', array());
-
             // Check if the edit action is present in the URL and if the add-on index is set
             if (isset($_GET['action']) && $_GET['action'] === 'edit' && isset($_GET['index'])) {
-                $index = intval($_GET['index']);
-
-                // Check if the index is within the valid range
-                if ($index >= 0 && $index < count($adicionais_data)) {
-                    $adicional_edit = $adicionais_data[$index];
-                    include(plugin_dir_path(__FILE__) . 'edit-additional.php');
-                    return;
-                }
+                include(plugin_dir_path(__FILE__) . 'edit-additional.php');
+                return;
             }
+
+            // Check if the add action is present in the URL
+            if (isset($_GET['action']) && $_GET['action'] === 'add') {
+                include(plugin_dir_path(__FILE__) . 'add-additional.php');
+                return;
+            }
+
+            $adicionais_data = get_option('reserva_veiculos_data_additionals', array());
         ?>
 
         <h2>Administrador de Adicionais</h2>
+
+        <a href="<?php echo admin_url('admin.php?page=reserva_veiculos_adicionais_page&action=add'); ?>" class="page-title">Adicionar Novo</a>
 
         <!-- display table is in the admin_menu -->
         <?php display_table($adicionais_data, ['Adicionais', 'Preço', 'Descrição'], 'reserva_veiculos_adicionais_page', 'name', 'price', 'description'); ?>
