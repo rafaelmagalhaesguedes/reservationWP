b460e98643e540fc101813ff251cbe8c9d41cbfb
rafael <rafaelmagalhaes1@live.com>
2024-02-15 10:57:36 -0300

    feat: add buttons edit and remove on tables

--- includes/admin/utils/display_table.php
+++ includes/admin/utils/display_table.php
@@ -1,11 +1,32 @@
 <?php
 function display_table($data, $headers, $edit_page, ...$fields) {
+    echo '
+    <style>
+        table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+        th, td {
+            padding: 10px;
+            text-align: left;
+            border-bottom: 1px solid #ddd;
+        }
+        th {
+            background-color: #f2f2f2;
+        }
+        .actions {
+            text-align: center;
+            width: 5%;
+        }
+    </style>';
+
     echo '<table class="wp-list-table widefat fixed striped">';
     echo '<thead><tr>';
     foreach ($headers as $header) {
         echo '<th scope="col">' . $header . '</th>';
     }
-    echo '<th scope="col">Editar</th>';
+    echo '<th class="actions" scope="col"></th>';
+    echo '<th  class="actions" scope="col"></th>';
     echo '</tr></thead>';
     echo '<tbody>';
     foreach ($data as $index => $item) {
@@ -13,7 +34,8 @@ function display_table($data, $headers, $edit_page, ...$fields) {
         foreach ($fields as $field) {
             echo '<td>' . esc_html($item[$field]) . '</td>';
         }
-        echo '<td><a href="?page=' . $edit_page . '&action=edit&index=' . $index . '">Editar</a></td>';
+        echo '<td><a href="?page=' . $edit_page . '&action=edit&index=' . $index . '"><span class="dashicons dashicons-edit"></span></a></td>';
+        echo '<td><a href="?page=' . $edit_page . '&action=delete&index=' . $index . '" onclick="return confirm(\'Deseja remover definitivamente?\')"><span class="dashicons dashicons-trash"></span></a></td>';
         echo '</tr>';
     }
     echo '</tbody></table>';
